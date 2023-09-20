250c5d5aeeab403970b2c196b0c06f976a2804a8
hiren979 <hirenchavda141@gmail.com>
2023-09-20 13:12:22 +0530

    Implement Login & Register

--- Minimal-chat-application-frontend/src/app/app-routing.module.ts
+++ Minimal-chat-application-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: 'home', component: HomeComponent,canActivate: [AuthGuard] },           
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent},
+  { path: '**', redirectTo: '/' }               
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Minimal-chat-application-frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    // Initialize the loginForm with form controls
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const user = this.loginForm.value;
+      this.authService.login(user).subscribe(
+        (response) => {
+          console.log('Login successful:', response);
+
+          localStorage.setItem('user', JSON.stringify(response));
+
+          this.toastr.success('Login successful!', 'Success');
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          alert("failed login");
+          this.toastr.error('Login failed!', 'Error');
+          console.log('Login failed:', error);
+        }
+      );
+    }
+  }
+}
